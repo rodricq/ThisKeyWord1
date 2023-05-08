9ba91483d65daacfeb1b1fccd6337c28cbbb4ae5
beverley-dick <beverleydick11@gmail.com>
2023-05-08 16:52:45 +0300

    Add project files.

---
+++ ThisKeyword2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ThisKeyword2
+{
+    class Test
+    {
+        int num1;
+        int num2;
+
+        Test()
+        {
+            num1 = 22;
+            num2 = 33;
+        }
+
+        //method that accepts this as argument
+        void passParameter(Test t1)
+        {
+            Console.WriteLine("num1: " + num1);
+            Console.WriteLine("num2: " + num2);
+        }
+
+        void display()
+        {
+            //passing this as a parameter
+            passParameter(this);
+        }
+
+        public static void Main(string[] args)
+        {
+            Test t1 = new Test();
+            t1.display();
+            Console.ReadLine();
+        }
+    }
+}
